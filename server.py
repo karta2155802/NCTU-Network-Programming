550ae7ed46afdc4771be6a4781014cd7746f51c8
fusy1019 <fusy1019@linux2.cs.nctu.edu.tw>
2020-03-20 23:35:05 +0800

    try

--- server.py
+++ server.py
@@ -5,12 +5,12 @@ import _thread
 def new_client(clientsocket, addr):
 	msg_out = 'Welcoome to the BBS server\r\n'
 	clientsocket.send(msg_out.encode('utf-8'))
+	msg_in = clientsocket.recv(1024)
 	while True:
 		msg_out = '%'
 		clientsocket.send(msg_out.encode('utf-8'))
-		msg_tmp = clientsocket.recv(1024)
-		msg_in = msg_tmp.decode('utf-8')
-		print(msg_tmp)
+		msg_in = clientsocket.recv(1024)
+		print(msg_in.decode('utf-8'))
 
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,5 +24,3 @@ while True:
 	clientsocket, addr = serversocket.accept();
 	print("New Connection: %s" %str(addr))
 	_thread.start_new_thread(new_client, (clientsocket, addr))
-
-	
