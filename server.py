cbe4d21c673460763997749c62eeca6c4ed6e6bd
fusy1019 <fusy1019@linux2.cs.nctu.edu.tw>
2020-03-20 16:39:56 +0800

    multi-client

--- server.py
+++ server.py
@@ -1,16 +1,29 @@
 import socket
 import sys
+import thread
 
-serversocket = socket.socket(socket.AFINET, sockket.SOCK_STEAM)
+def new_client(clientsocket, addr):
+	msg_out = 'Welcoome to the BBS server'
+	clientsocket.send(msg_out.encode('utf-8'))
+	while True:
+		msg_out = '%'
+		clientsocket.send(msg_out.encode('utf-8'))
+		msg_in = s.recv(1024)
+		print(msg_in.decode('utf-8'))
+
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname();
 port = 10000
 serversocket.bind((host, port))
 serversocket.listen(11)
 
-while Ture:
+while True:
 	clientsocket, addr = serversocket.accept();
 	print("New Connection: %s" %str(addr))
+	thread.start_new_thread(new_client, (clientsocket, addr))
 
-	msg = 'Welcoome to the BBS server'
-	clientsocket.send(msg.encode('utf-8'))
-	clientsocket.close()
+	
+	
+	
+	msg_in = s.recv(1024)
