e6dee27b445559def8ad39749c62117fa7ac21d7
fusy1019 <fusy1019@linux2.cs.nctu.edu.tw>
2020-03-22 00:25:53 +0800

    register

--- Lab1/server.py
+++ Lab1/server.py
@@ -5,10 +5,10 @@ import sqlite3
 from sqlite3 import Error
 
 
-def sql_insert(msg_in, conn, c):
+def sql_insert(msg_list, conn, c):
 	try:
-		msg_split = msg_in.split()
-		reply=c.execute('insert into USERS ("Username", "Email", "Password") values (?,?,?)', (msg_split[1], msg_split[2], msg_split[3]))
+		#msg_split = msg_in.split()
+		c.execute('insert into USERS ("Username", "Email", "Password") values (?,?,?)', (msg_list[1], msg_list[2], msg_list[3]))
 		conn.commit()
 		print('Insertion success')
 	except Error:
@@ -24,36 +24,32 @@ def new_client(clientsocket, addr):
 	msg_out = 'Welcoome to the BBS server\r\n'
 	clientsocket.send(msg_out.encode('utf-8'))
 	clientsocket.recv(1024)
-	#msg_out = '% '
-	#clientsocket.send(msg_out.encode('utf-8'))
-	msg_in = 'initial'
+
 	while True:
-		if msg_in != '':
-			msg_out = '% '
-			clientsocket.send(msg_out.encode('utf-8'))
+		msg_out = '% '
+		clientsocket.send(msg_out.encode('utf-8'))
 		msg_in = clientsocket.recv(1024).decode('utf-8')
 		print(msg_in)
 		msg_in = msg_in.replace('\r','').replace('\n','')			
 		
-		#msg_list = msg_in.split();	
+		msg_list = msg_in.split();	
 		try:
-			string_processing(msg_in, conn, c)
+			string_processing(msg_list, conn, c)
 		except:
 			continue
-		#msg_out = '% '
-		#clientsocket.send(msg_out.encode('utf-8'))
+
 		
 
 
-def string_processing(msg_in, conn, c):
-	msg_split = msg_in.split()
-	if msg_split[0] == "register":
-		if len(msg_split) != 4:
+def string_processing(msg_list, conn, c):
+	#msg_split = msg_in.split()
+	if msg_list[0] == "register":
+		if len(msg_list) != 4:
 			msg_out = 'Usage: regoster <username> <email> <password>\r\n'
 			clientsocket.send(msg_out.encode('utf-8'))
 		else:
 			print('Inserting...')
-			sql_insert(msg_in, conn, c)
+			sql_insert(msg_list, conn, c)
 
 
 	#elif str[0] == "login":
