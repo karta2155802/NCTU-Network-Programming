9014850600ff70a94c063711247aea73f072986d
fusy1019 <fusy1019@linux2.cs.nctu.edu.tw>
2020-03-21 18:36:31 +0800

    sql insert

--- Lab1/server.py
+++ Lab1/server.py
@@ -1,20 +1,66 @@
 import socket
 import sys
 import _thread
+import sqlite3
+from sqlite3 import Error
+
+
+
+def sql_connection():
+	try:
+		con = sqlite3.connect('Database.db')
+		return con
+	except Error:
+		print(Error)
+
+def sql_insert(str):
+	try:
+		cursorObj.execute("insert into  USERS (Username, Email, Password) values (?,?,?)", (str[1], str[2], str[3]))
+	except Error:
+		print(Error)
+		msg_out = 'Username is already used.'
+		clientsocket.send(msg_out.encode('utf-8'))
 
 def new_client(clientsocket, addr):
 	msg_out = 'Welcoome to the BBS server\r\n'
-	clientsocket.send(msg_out.encode('utf-8'))
+	
 	clientsocket.recv(1024)
 	msg_in = 'initial'
 	while True:
 		if msg_in != '': 
-			msg_out = '%'
+			msg_out = '% '
 			clientsocket.send(msg_out.encode('utf-8'))
 		msg_in = clientsocket.recv(1024).decode('utf-8')
 		msg_in = msg_in.replace('\r','').replace('\n','')
 		print(msg_in)
+		msg_list  = msg_in.split();
+		string_processing(msg_list)
 
+
+def string_processing(str):
+	if str[0] == 'register':
+		if(len(str != 4)):
+			msg_out = 'Usage: regoster <username> <email> <password>'
+			clientsocket.send(msg_out.encode('utf-8'))
+		else:
+			sql_insert(str)
+
+
+	elif str.split()[0] == 'login':
+		if(len(str.split() != 3)):
+			msg_out = 'Usage: login <username> <password>'
+			clientsocket.send(msg_out.encode('utf-8'))
+		else:
+
+	elif str.split()[0] == 'logout':
+
+	elif str.split()[0] == 'whoami':
+
+	elif str.split()[0] == 'exit':
+
+
+con = sql_connection()
+cursorObj = con.consor()
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname();
