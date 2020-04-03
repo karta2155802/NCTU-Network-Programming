fed9177d982779a4db15ceff1479f85f5fd277ea
karta2155802 <karta2155802@gmail.com>
2020-04-03 23:28:15 +0800

    list board

--- Lab2/server.py
+++ Lab2/server.py
@@ -5,8 +5,17 @@ import sqlite3
 import time
 from sqlite3 import Error
 
+def sql_list_board(c, uid, keyword):
+	sql_return = c.execute('select BOARD.ID, BOARD.Name, USERS.Username from BOARD inner join USERS on BOARD.Moderator_id = USERS.UID where BOARD.Name like ?', (keyword,))
+	msg_out = '\r\n   {:<7} {:^20} {:^20}\r\n'.format("Index", "Name", "Moderator")
+	clientsocket.send(msg_out.encode('utf-8'))
+	for row in sql_return:
+		msg_out = '   {:<7} {:^20} {:^20}\r\n'.format(row[0], row[1], row[2])
+		ClientSocket.send(msg_out.encode('utf-8'))
+	print('List board successfully')
+
 def sql_create_post(conn, c, uid, data):
-	sql_return = c.execute('select * from BOARD where Name = ?',(data[0],)).fetchone()
+	sql_return = c.execute('select * from BOARD where Name = ?', (data[0],)).fetchone()
 	if(sql_return == None):
 		print('Board is not exist')
 		msg_out = 'Board is not exist.\r\n'
@@ -34,12 +43,12 @@ def sql_create_board(msg_list, conn, c, uid):
 		clientsocket.send(msg_out.encode('utf-8'))
 
 def sql_whoami(c, uid):
-	sql_return = c.execute('select * from USERS where UID = ?',(uid,)).fetchone()
+	sql_return = c.execute('select * from USERS where UID = ?', (uid,)).fetchone()
 	msg_out = sql_return[1] + '\r\n'
 	clientsocket.send(msg_out.encode('utf-8'))
 
 def sql_logout(msg_list, c, uid):
-	sql_return = c.execute('select * from USERS where UID = ?',(uid,)).fetchone()
+	sql_return = c.execute('select * from USERS where UID = ?', (uid,)).fetchone()
 	msg_out = 'Bye, ' + sql_return[1] + '.\r\n'
 	clientsocket.send(msg_out.encode('utf-8'))
 	uid = -1
@@ -47,7 +56,7 @@ def sql_logout(msg_list, c, uid):
 	return uid
 
 def sql_login(msg_list, c, uid):
-	sql_return = c.execute('select * from USERS where Username = ?',(msg_list[1],)).fetchone()
+	sql_return = c.execute('select * from USERS where Username = ?', (msg_list[1],)).fetchone()
 	if sql_return != None and sql_return[3] == msg_list[2]:
 		uid = sql_return[0]
 		print(msg_list[1],'has login')
@@ -71,6 +80,7 @@ def sql_register(msg_list, conn, c):
 		clientsocket.send(msg_out.encode('utf-8'))
 
 def string_processing(msg_in, conn, c, uid):
+	hashtag = "##"
 	msg_list = msg_in.split();
 	print(uid)
 	if msg_list[0] == 'register':
@@ -92,7 +102,7 @@ def string_processing(msg_in, conn, c, uid):
 			uid = sql_login(msg_list, c, uid)
 	elif msg_list[0] == 'logout':
 		if uid == -1:
-			msg_out = 'Please login first\r\n'
+			msg_out = 'Please login first.\r\n'
 			clientsocket.send(msg_out.encode('utf-8'))
 		if len(msg_list) != 1:
 			msg_out = 'Usage: logout\r\n'
@@ -124,7 +134,7 @@ def string_processing(msg_in, conn, c, uid):
 		if uid == -1:
 			msg_out = 'Please login first.\r\n'
 			clientsocket.send(msg_out.encode('utf-8'))
-		elif len(msg_list)>5 and msg_list[2] == '--title' and '--content' in msg_in:
+		elif len(msg_list) > 5 and msg_list[2] == '--title' and '--content' in msg_in:
 			board_name = msg_list[1]
 			print('board_name =', board_name)
 			content_position = msg_list.index('--content')
@@ -142,6 +152,21 @@ def string_processing(msg_in, conn, c, uid):
 		else:
 			msg_out = 'Usage: create-post <board-name> --title <title> --content <content>\r\n'
 			clientsocket.send(msg_out.encode('utf-8'))
+	elif msg_list[0] == 'list-board':
+		if uid == -1:
+			msg_out = 'Please login first.\r\n'
+			clientsocket.send(msg_out.encode('utf-8'))
+		elif hashtag in msg_in and len(msg_list) == 2:
+			keyword = msg_list[1].replace('##','')
+			print('keyword =', keyword)
+			sql_list_board(c, uid, keyword)
+
+		##elif len(msg_list) == 1:
+
+		else:
+			msg_out = 'Usage: list-board ##<key>\r\n'
+			clientsocket.send(msg_out.encode('utf-8'))
+
 	return uid
 
 def new_client(clientsocket, addr):
