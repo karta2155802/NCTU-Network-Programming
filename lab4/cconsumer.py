4c6e366d5293ab08488c5b1fb90933542fa1c0c5
karta2155802 <karta2155802@gmail.com>
2020-05-26 01:07:03 +0800

    test

--- lab4/cconsumer.py
+++ lab4/cconsumer.py
@@ -1,11 +1,12 @@
 from kafka import KafkaConsumer
 import threading
+import time
 # 使用group,對於同一個group的成員只有一個消費者例項可以讀取資料
 
 def consume():
     while True:
-        consumer = KafkaConsumer(bootstrap_servers=['127.0.0.1:9092'])
-        consumer.subscribe(topics=('test2'))  #訂閱要消費的主題
+        consumer = KafkaConsumer(group_id = 'B',bootstrap_servers=['127.0.0.1:9092'])
+        consumer.subscribe(topics=('test'))  #訂閱要消費的主題
         for message in consumer:
             print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,message.offset, message.key,message.value))
 
--- lab4/consumer.py
+++ lab4/consumer.py
@@ -1,10 +1,11 @@
 from kafka import KafkaConsumer
 import threading
+import time
 # 使用group,對於同一個group的成員只有一個消費者例項可以讀取資料
 
 def consume():
-    while:
-        consumer = KafkaConsumer(bootstrap_servers=['127.0.0.1:9092'])
+    while True:
+        consumer = KafkaConsumer(group_id = 'A', bootstrap_servers=['127.0.0.1:9092'])
         consumer.subscribe(topics=('test'))  #訂閱要消費的主題
         for message in consumer:
             print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,message.offset, message.key,message.value))
